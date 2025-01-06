81650ddb35fbf8f1f303debfb51893ac6b318a01
suuny0321 <joosun7647@naver.com>
2025-01-06 10:38:05 +0900

    controller response

---
+++ src/main/java/com/ll/chatai/ChatController.java
@@ -0,0 +1,29 @@
+package com.ll.chatai;
+
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RequestMapping("/api/vi/chat")
+@RestController
+public class ChatController {
+    private final OpenAiChatModel openAiChatModel;
+
+    public ChatController(OpenAiChatModel openAiChatModel) {
+        this.openAiChatModel = openAiChatModel;
+    }
+
+    @GetMapping("/ai")
+    public Map<String, String> chat(@RequestBody String message) {
+        Map<String, String> responses = new HashMap<>();
+
+        String openAiRessponse =openAiChatModel.call(message);
+        responses.put("OpenAI -ChatGPT 응답", openAiRessponse);
+        return responses;
+    }
+
+
+}
